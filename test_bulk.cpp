fbd2cb8e97ba7f8f8478008fd5544baedeef4098
= <=>
2025-07-14 00:13:57 +0700

    feature: реализация тестов

--- test_bulk.cpp
+++ test_bulk.cpp
@@ -5,8 +5,9 @@
 #ifndef __PRETTY_FUNCTION__
 #include "pretty.h"
 #endif
-#include "bulk.h"
+#include "bulk_internal.h"
 
+using namespace otus_hw7;
 
 int main(int argc, char **argv)
 {
@@ -16,8 +17,21 @@ int main(int argc, char **argv)
 
 using namespace std::literals::string_literals;
 
-TEST(test_bulk, test_create)
+TEST(test_bulk, test_q)
 {
-    EXPECT_TRUE( std::cout );
+    ICommandQueuePtr_t cmd_q = create_command_queue();
+    EXPECT_TRUE( cmd_q );
+    cmd_q->push(CommandCreator().create_command("Test data"));
+    EXPECT_EQ(cmd_q->size(), 1);
+    ICommandPtr_t cmd = CommandCreator().create_command("Test data 2");
+    cmd_q->push(std::move(cmd));
+    EXPECT_EQ(cmd_q->size(), 2);
+    cmd_q->pop(cmd);
+    EXPECT_EQ(cmd_q->size(), 1);
 }
 
+TEST(test_bulk, test_create_q)
+{
+    ICommandQueuePtr_t cmd_q = create_command_queue();
+    EXPECT_TRUE( cmd_q );
+}
